4bfc3896292b6f8e930c2c80686556753cd2b904
Alvaro Pinot <alvaro@neatnait.com>
2024-05-06 09:58:03 +0200

    fix(Products): Ensure id is passed down to kv search

--- routes/api/users/[id].ts
+++ routes/api/users/[id].ts
@@ -12,8 +12,18 @@ const kv = await Deno.openKv()
 
 export const handler: Handlers<Product | null> = {
   async GET(_req, ctx) {
+    // const id = ctx.params.id
+    // // const user = { id: 2, name: 'foo' }
+    // const userKey = ['user', id]
+    // // const userRes = await kv.get(userKey)
+    // // if (!userRes.value) return new Response(`no user with id ${id} found`)
+    // const ok = await kv.atomic().set(userKey, user).commit()
+    // if (!ok) throw new Error('Something went wrong.')
+    // return new Response(JSON.stringify(user))
     const id = ctx.params.id
-    const product = get({ kv, key: id })
+    // const key = ['Products', id]
+    const product = await kv.get(['Products', id])
+    // const product = (await kv.get<Product>(key)).gvalue!
     return new Response(JSON.stringify(product))
   },
   async DELETE(_req, ctx) {
